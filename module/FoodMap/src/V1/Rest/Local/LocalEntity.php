0379c142d0330ace766e9383260664c5d689c5b2
Paulo Henrique Tameirão Fonseca Araújo de Oliveira <paulo.fonseca@itamaraty.gov.br>
2020-11-13 05:30:11 -0300

    Criação da Entity Local para mapear os campos da tabela de mesmo nome.

--- module/FoodMap/src/V1/Rest/Local/LocalEntity.php
+++ module/FoodMap/src/V1/Rest/Local/LocalEntity.php
@@ -3,4 +3,39 @@ namespace FoodMap\V1\Rest\Local;
 
 class LocalEntity
 {
+    public $id;
+    public $latitude;
+    public $longitude;
+    public $observacao;
+    public $dtCadastro;
+    public $usuarioCadastro;
+    public $dtAlteracao;
+    public $usuarioAlteracao;
+
+    public function getArrayCopy()
+    {
+        return [
+            'id'                => $this->id,
+            'latitude'          => $this->latitude,
+            'longitude'         => $this->longitude,
+            'observacao'        => $this->observacao,
+            'dtCadastro'        => $this->dtCadastro,
+            'usuarioCadastro'   => $this->usuarioCadastro,
+            'dtAlteracao'       => $this->dtAlteracao,
+            'usuarioAlteracao'  => $this->usuarioAlteracao,
+        ];
+    }
+
+    public function exchangeArray(array $array)
+    {
+		$this->id                   = $array['id'];
+		$this->latitude         = $array['latitude'];
+		$this->longitude        = $array['longitude'];
+		$this->observacao       = $array['observacao'];
+		$this->dtCadastro       = $array['dtCadastro'];
+		$this->usuarioCadastro  = $array['usuarioCadastro'];
+		$this->dtAlteracao      = $array['dtAlteracao'];
+		$this->usuarioAlteracao = $array['usuarioAlteracao'];
+    }
+
 }
