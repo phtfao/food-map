2f12a5c1a576dd01b4a8515194c16a254489d9f0
Paulo Henrique Tameirão Fonseca Araújo de Oliveira <paulo.fonseca@itamaraty.gov.br>
2020-11-14 14:18:54 -0300

    Atualização do banco de dados.

--- data/foodmap.sql
+++ data/foodmap.sql
@@ -5,7 +5,5 @@ CREATE TABLE local (
     longitude decimal(2, 10) NOT NULL, 
     observacao text, 
     dt_cadastro text(10) NOT NULL,
-    usuario_cadastro text(30),
-    dt_alteracao text(10),
-    usuario_alteracao text(30)
+    dt_alteracao text(10)
 );
