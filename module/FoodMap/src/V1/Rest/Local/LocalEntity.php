9751caa848003650388adfa619650e4f8e030e2d
Paulo Henrique Tameirão Fonseca Araújo de Oliveira <paulo.fonseca@itamaraty.gov.br>
2020-11-14 14:20:59 -0300

    Refactory da entity e remoção de campos que não serão utilizados.

--- module/FoodMap/src/V1/Rest/Local/LocalEntity.php
+++ module/FoodMap/src/V1/Rest/Local/LocalEntity.php
@@ -1,41 +1,90 @@
 <?php
 namespace FoodMap\V1\Rest\Local;
 
+use Laminas\Hydrator;
+
 class LocalEntity
 {
     public $id;
     public $latitude;
     public $longitude;
     public $observacao;
-    public $dtCadastro;
-    public $usuarioCadastro;
-    public $dtAlteracao;
-    public $usuarioAlteracao;
+    public $dt_cadastro;
+    public $dt_alteracao;
+
+    public function __construct($data = array())
+    {
+        $this->exchangeArray($data);
+    }
 
     public function getArrayCopy()
     {
         return [
-            'id'                => $this->id,
-            'latitude'          => $this->latitude,
-            'longitude'         => $this->longitude,
-            'observacao'        => $this->observacao,
-            'dtCadastro'        => $this->dtCadastro,
-            'usuarioCadastro'   => $this->usuarioCadastro,
-            'dtAlteracao'       => $this->dtAlteracao,
-            'usuarioAlteracao'  => $this->usuarioAlteracao,
+            'id' => $this->getId(),
+            'latitude' => $this->getLatitude(),
+            'longitude' => $this->getLongitude(),
+            'observacao' => $this->getObservacao(),
+            'dt_cadastro' => $this->getDtCadastro(),
+            'dt_alteracao' => $this->getDtAlteracao(),
         ];
     }
 
-    public function exchangeArray(array $array)
+    public function exchangeArray(array $data)
     {
-		$this->id                   = $array['id'];
-		$this->latitude         = $array['latitude'];
-		$this->longitude        = $array['longitude'];
-		$this->observacao       = $array['observacao'];
-		$this->dtCadastro       = $array['dtCadastro'];
-		$this->usuarioCadastro  = $array['usuarioCadastro'];
-		$this->dtAlteracao      = $array['dtAlteracao'];
-		$this->usuarioAlteracao = $array['usuarioAlteracao'];
+        (new Hydrator\ClassMethodsHydrator)->hydrate($data, $this);
     }
 
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getLatitude() {
+        return $this->latitude;
+    }
+
+    public function setLatitude($latitude) {
+        $this->latitude = $latitude;
+        return $this;
+    }
+
+    public function getLongitude() {
+        return $this->longitude;
+    }
+
+    public function setLongitude($longitude) {
+        $this->longitude = $longitude;
+        return $this;
+    }
+
+    public function getObservacao() {
+        return $this->observacao;
+    }
+
+    public function setObservacao($observacao) {
+        $this->observacao = $observacao;
+        return $this;
+    }
+
+    public function getDtCadastro() {
+        return $this->dt_cadastro;
+    }
+
+    public function setDtCadastro($dt_cadastro) {
+        $this->dt_cadastro = $dt_cadastro;
+        return $this;
+    }
+
+    public function getDtAlteracao() {
+        return $this->dt_alteracao;
+    }
+
+    public function setDtAlteracao($dt_alteracao) {
+        $this->dt_alteracao = $dt_alteracao;
+        return $this;
+    }
 }
